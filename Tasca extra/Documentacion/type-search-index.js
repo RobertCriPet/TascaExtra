dda058104ca199dc95e6b8cbfb4f4669bacdeca4
RobertCriPet <robpetpet@alu.edu.gva.es>
2026-03-15 10:57:22 +0100

    javaDOC generado

---
+++ Tasca extra/Documentacion/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ConcursoCalculoMental"},{"p":"<Unnamed>","l":"PruebaSudoku"},{"p":"<Unnamed>","l":"Sudoku"}];updateSearchResults();
